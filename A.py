b84ab27c80058ea28464f7837033d7e4b38cb478
15715733581 <1347274664@qq.com>
2020-10-26 20:54:42 +0800

    python 广播的一个例子

--- A.py
+++ A.py
@@ -4,4 +4,10 @@ array = np.array([[56, 0, 4.4, 68],
                  [1.2, 104, 52, 8],
                  [1.8, 135, 99, 0.9]])
 
-print(array)
+# print(array)
+cal1 = array.sum(axis=0)
+print(cal1)
+cal2 = array.sum(axis=1)
+print(cal2)
+percentage = 100*array/cal1.reshape(1, 4)
+print(percentage)
